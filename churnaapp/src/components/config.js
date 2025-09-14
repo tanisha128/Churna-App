904d5599c0a2d4a0f1b24c7e7214b169f85e5ab8
tanisha128 <tanishateli18@gmail.com>
2025-09-15 00:30:11 +0530

    Changed config 3

--- churnaapp/src/components/config.js
+++ churnaapp/src/components/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const BASE_URL =
   process.env.MODE === "production"
-    ? process.env.VITE_BACKEND_URL  // from .env in production
+     ? "" // same domain in production 
     : "http://localhost:5000";            // local dev backend
 
 export const API_URL = `${BASE_URL}/api`;
