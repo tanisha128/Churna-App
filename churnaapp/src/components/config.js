a1e710a149bace1eace7369fee7a8d3b6ca279fb
tanisha128 <tanishateli18@gmail.com>
2025-10-06 18:36:39 +0530

    changed config

--- churnaapp/src/components/config.js
+++ churnaapp/src/components/config.js
@@ -1,6 +1,4 @@
-export const BASE_URL =
-  import.meta.env.MODE === "production"
-    ? ""
-    : "http://localhost:5000"; // match backend port
-
+// config.js
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"; 
 export const API_URL = `${BASE_URL}/api`;
+
